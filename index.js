9bedbe9b2440d44ba57d7e86caa3c0449241217d
zacboz <zacboz@gmail.com>
2017-03-02 19:32:58 -0700

    finished node part of todo

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var mainCtrl = require('./mainCtrl.js');
 var app = express();
 
-
-var app = express();
 
 app.use(bodyParser.json());
 app.use(express.static(__dirname + "/public"));
 
-
+app.post('/todo', mainCtrl.addTodo);
+app.get('/todo', mainCtrl.getTodo);
+app.put('/completetodo/:id', mainCtrl.completeTodo);
+app.put('/uncompletetodo/:id', mainCtrl.uncompleteTodo);
+app.delete('/todo/:id', mainCtrl.deleteTodo);
 
 
 var port = 3000;
---
+++ mainCtrl.js
@@ -0,0 +1,45 @@
+var todos = require('./todos.js');
+var uuidV4 = require('uuid/v4');
+
+module.exports = {
+
+  addTodo: function(req, res){
+    var newTodo = {
+      task: req.body.task,
+      complete: req.body.complete,
+      id: uuidV4()
+    };
+    todos.push(newTodo);
+    console.log('adding todos', todos);
+    res.status(200).json(todos)
+  },
+
+  getTodo: function(req, res){
+    console.log('getting todos', todos);
+    res.status(200).json(todos);
+  },
+
+  completeTodo: function(req, res) {
+    let index = todos.findIndex(todo => todo.id === req.params.id);
+    todos[index].complete = true;
+    console.log('complete todos', todos);
+    res.status(200).json(todos)
+  },
+
+  uncompleteTodo: function(req, res) {
+    let index = todos.findIndex(todo => todo.id === req.params.id);
+    console.log(index);
+    todos[index].complete = false;
+    console.log('uncomplete todos', todos);
+    res.status(200).json(todos)
+  },
+
+  deleteTodo: function(req, res) {
+    let index = todos.findIndex(todo => todo.id === req.params.id);
+    todos.splice(index, 1);
+    console.log('delete todos', todos);
+    res.status(200).json(todos);
+  }
+
+
+}
