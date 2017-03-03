09461844f7bcf4e871dc0c232b5a72ce4f77fa9d
John Munson <johncmunson91@gmail.com>
2017-03-02 18:35:20 -0700

    add sample data

---
+++ todos.js
@@ -0,0 +1,27 @@
+let todos = [
+    {
+        task: 'mow the lawn',
+        complete: true,
+        id: '234wef'
+    },
+    {
+        task: 'do my homework',
+        complete: false,
+        id: 'c234c5'
+    },
+    {
+        task: 'win da hackathon',
+        complete: false,
+        id: 'b7r68'
+    },
+    {
+        task: 'eat food',
+        complete: true,
+        id: '67hrd'
+    },
+    {
+        task: 'graduate from devmtn',
+        complete: false,
+        id: '65en7r'
+    }
+]
